1ff99e9ebcf8fa7fd8dfce25bcc416f8a7a607c8
IkhsanHadi <ikhsanhdn747@gmail.com>
2024-07-25 15:17:45 +0700

    update meeting 2024-07-24

--- Controllers/DebitBendungController.cs
+++ Controllers/DebitBendungController.cs
@@ -18,6 +18,10 @@ namespace IrigasiManganti.Controllers
       _unitOfWorkRepository = unitOfWorkRepository;
     }
 
+    public IActionResult Index() {
+      return View();
+    }
+
     [HttpPost]
     public async Task<IActionResult> GetDatatableByRangeDate()
     {
@@ -48,6 +52,45 @@ namespace IrigasiManganti.Controllers
             ModelRequest.Skip = ModelRequest.Start != null ? Convert.ToInt32(ModelRequest.Start) : 0;
 
             var (debits, recordsTotal) = await _unitOfWorkRepository.DebitBendungs.GetDatatableByRangeDateAsync(ModelRequest);
+            var jsonData = new { draw = ModelRequest.Draw, recordsFiltered = recordsTotal, recordsTotal, data = debits };
+            return Json(jsonData);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "General Exception: {@ExceptionDetails}", new { ex.Message, ex.StackTrace, DatatableRequest = ModelRequest });
+            throw;
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetDataAll()
+    {
+        var ModelRequest = new JqueryDataTableRequest
+        {
+            Draw = Request.Form["draw"].FirstOrDefault() ?? "",
+            Start = Request.Form["start"].FirstOrDefault() ?? "",
+            Length = Request.Form["length"].FirstOrDefault() ?? "",
+            SortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault() ?? "",
+            SortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault() ?? "",
+            SearchValue = Request.Form["search[value]"].FirstOrDefault() ?? ""
+        };
+
+        try
+        {
+            // Check if page size is set to show all
+            if (ModelRequest.Length == "-1")
+            {
+                // Set page size to a large number or adjust your data retrieval logic accordingly
+                ModelRequest.PageSize = int.MaxValue;
+            }
+            else
+            {
+                ModelRequest.PageSize = ModelRequest.Length != null ? Convert.ToInt32(ModelRequest.Length) : 0;
+            }
+
+            ModelRequest.Skip = ModelRequest.Start != null ? Convert.ToInt32(ModelRequest.Start) : 0;
+
+            var (debits, recordsTotal) = await _unitOfWorkRepository.DebitBendungs.GetDataAllAsync(ModelRequest);
             var jsonData = new { draw = ModelRequest.Draw, recordsFiltered = recordsTotal, recordsTotal, data = debits };
             return Json(jsonData);
         }
--- Repositories/DebitBendungRepository.cs
+++ Repositories/DebitBendungRepository.cs
@@ -15,6 +15,7 @@ namespace IrigasiManganti.Repositories
     {
         Task<(IReadOnlyList<dynamic>, int)> GetDatatableByRangeDateAsync2(JqueryDataTableRequestDebitBendung request);
         Task<(IReadOnlyList<dynamic>, int)> GetDatatableByRangeDateAsync(JqueryDataTableRequestDebitBendung request);
+        Task<(IReadOnlyList<dynamic>, int)> GetDataAllAsync(JqueryDataTableRequest request);
     }
     public class DebitBendungRepository : IDebitBendungRepository
     {
@@ -182,6 +183,77 @@ namespace IrigasiManganti.Repositories
             catch (Exception ex)
             {
                 Log.Error(ex, "General Exception: {@ExceptionDetails}", new { ex.Message, ex.StackTrace, Request = request });
+                throw;
+            }
+        }
+
+        public async Task<(IReadOnlyList<dynamic>, int)> GetDataAllAsync(JqueryDataTableRequest request) {
+            try {
+                using var connection = new NpgsqlConnection(_connectionString);
+                List<dynamic> result = new List<dynamic>();
+
+                var query = $@"SELECT 
+                    db.id,
+                    db.tanggal,
+                    db.ketersediaan_min AS ""ketersediaanMin"",
+                    db.ketersediaan_max AS ""ketersediaanMax"",
+                    db.kebutuhan,
+                    db.ketersediaan_avg AS ""ketersediaanAvg""
+                FROM 
+                    debit_bendung AS db
+                ORDER BY
+                    tanggal";
+
+                var parameters = new DynamicParameters();
+                var whereConditions = new List<string>();
+
+                if (!string.IsNullOrEmpty(request.SearchValue))
+                {
+                    if (request.SearchValue.Contains('\''))
+                    {
+                        request.SearchValue = request.SearchValue.Replace("'", "''");
+                    }
+
+                    if (request.SearchValue.Contains('['))
+                    {
+                        request.SearchValue = request.SearchValue.Replace("[", "''");
+                    }
+
+                    whereConditions.Add(@"
+                    (LOWER(tanggal) LIKE @SearchValue");
+                    parameters.Add("@SearchValue", "%" + request.SearchValue.ToLower() + "%");
+                }
+
+                var whereClause = whereConditions.Count > 0 ? "WHERE " + string.Join(" AND ", whereConditions) : "";
+
+                query += whereClause;
+
+                int total = 0;
+                var sql_count = $"SELECT COUNT(*) FROM ({query}) as total";
+                total = connection.ExecuteScalar<int>(sql_count, parameters);
+
+                // if (!(string.IsNullOrEmpty(request.SortColumn) && string.IsNullOrEmpty(request.SortColumnDirection)))
+                // {
+                //     query = query + @$" ORDER BY CASE WHEN ""LastReadingAt"" IS NULL THEN 1 ELSE 0 END, ""{request.SortColumn}""" + " " + request.SortColumnDirection;
+                // }
+                // else
+                // {
+                //     query = query + @" ORDER BY CASE WHEN ""LastReadingAt"" IS NULL THEN 1 ELSE 0 END, ""DeviceStatus"" DESC, ""LastReadingAt"" DESC";
+                // }
+
+                query += @" 
+                OFFSET @Skip ROWS FETCH NEXT @PageSize ROWS ONLY;";
+                parameters.Add("@Skip", request.Skip);
+                parameters.Add("@PageSize", request.PageSize);
+
+                result = (await connection.QueryAsync<dynamic>(query, parameters)).ToList();
+
+                return (result, total);
+            } catch (Npgsql.NpgsqlException ex) {
+                Log.Error(ex, "Sql Exception: {@ExceptionDetails}", new { ex.Message, ex.StackTrace, Desc = "Error while get data to table petak" });
+                throw;
+            } catch (System.Exception ex) {
+                Log.Error(ex, "General Exception: {@ExceptionDetails}", new { ex.Message, ex.StackTrace, Desc = "Error while get data to table petak" });
                 throw;
             }
         }
